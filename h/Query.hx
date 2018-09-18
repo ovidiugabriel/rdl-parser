43d7e78f018ccca470be0ffb3af986fde2138f12
Ovidiu <ovidiugabriel@gmail.com>
2018-09-18 16:11:27 +0300

    Update Query.hx

--- h/Query.hx
+++ h/Query.hx
@@ -1,8 +1,12 @@
 
 class Query {
-    var CommandText;
-    var CommandType;
-    var DataSourceName;
-    var QueryParameters;
-    var Timeout;
+    var CommandText : String;
+
+    // "Text" - element contains a query command to execute.
+    // "StoredProcedure" - element contains the name of a stored procedure to execute.
+    // "TableDirect" - element contains the name of a Table from which to retrieve rows.
+    var CommandType : String;
+    var DataSourceName : String;
+    var QueryParameters : QueryParameters;
+    var Timeout : Int;
 }
